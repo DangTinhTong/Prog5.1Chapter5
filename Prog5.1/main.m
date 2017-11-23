2a0dd277f3e5e840a7e1714ca46cafb7ec0ab1da
admin <dangtinhtong@gmail.com>
2017-11-23 14:26:24 +0700

    First commit

--- Prog5.1/main.m
+++ Prog5.1/main.m
@@ -2,16 +2,19 @@
 //  main.m
 //  Prog5.1
 //
-//  Created by admin on 11/23/17.
-//  Copyright © 2017 admin. All rights reserved.
+//  Created by Tống Đăng Tình on 11/23/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
+// Program to calcualte the eigth triangular number
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        int triangularNumber;
+        triangularNumber = 1+2+3+4+5+6+7+8;
+        NSLog(@"THe eigth triangulator number is %i", triangularNumber);
     }
     return 0;
 }
